92ebdd62f33669623c7c03a66c768b6fa7c1a68f
Ronkleinhause <rkleinhauseg@umass.edu>
2024-11-21 00:24:42 -0500

    auth qith keys in .env

--- Backend/auth/auth.js
+++ Backend/auth/auth.js
@@ -1,6 +1,19 @@
 import { initializeApp } from 'firebase-admin/app';
-import credentials from "./config/adminKey.json" with {type: 'json'};
-
+const dotenv = require('dotenv');
+dotenv.config();
+const credentials = {
+  type: process.env.FIREBASE_API_TYPE,
+  project_id: process.env.FIREBASE_API_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_API_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_API_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_API_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_API_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_API_AUTH_URI,
+  token_uri: process.env.FIREBASE_API_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_API_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_API_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_API_UNIVERSE_DOMAIN
+}
 admin.initializeApp({
     credential: admin.credential.cert(credentials)
 });
